fab1b2ba7d3df1f8514e45d17eb6aee4f92e5ce3
Mmari00 <1software.files1@gmail.com>
2024-11-21 17:48:23 -0500

    Login e imagenes listas

---
+++ public/js/login.js
@@ -0,0 +1,27 @@
+const container = document.getElementById('container');
+const registerBtn = document.getElementById('register');
+const loginBtn = document.getElementById('login');
+const register = document.getElementById('uno');
+const login = document.getElementById('dos');
+
+registerBtn.addEventListener('click', () => {
+    container.classList.add("active");
+    
+});
+
+loginBtn.addEventListener('click', () => {
+    container.classList.remove("active");
+    
+});
+
+register.addEventListener('click', () => {
+    alert('¡Registro exitoso! Bienvenid@ a nuestra plataforma.');
+    
+});
+
+login.addEventListener('click', () => {
+    alert('¡Ingreso exitoso! Bienvenid@ otra vez.');
+    
+});
+
+
